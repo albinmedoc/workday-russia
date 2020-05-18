9abb5512c3e76cfee12602104980f4b0bc3ddfa1
Albin Médoc <stamline203@gmail.com>
2020-05-19 01:32:17 +0200

    Added style-loader & css-loader

--- webpack.config.js
+++ webpack.config.js
@@ -6,4 +6,13 @@ module.exports = {
         path: path.resolve(__dirname, "dist"),
         filename: "bundle.js",
     },
+    mode: "development",
+    module: {
+        rules: [
+            {
+                test: /\.css$/i,
+                use: ["style-loader", "css-loader"],
+            },
+        ],
+    },
 };
