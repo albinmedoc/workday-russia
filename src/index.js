c82341d134255cac8c805d33fbfcc852e1e8d4c4
Albin Médoc <stamline203@gmail.com>
2020-05-19 13:28:46 +0200

    Fetch holiday bool of clicked date & console log it

--- src/index.js
+++ src/index.js
@@ -1,15 +1,34 @@
 import { Calendar } from "@fullcalendar/core";
 import dayGridPlugin from "@fullcalendar/daygrid";
+import interactionPlugin from "@fullcalendar/interaction";
 
 import "@fullcalendar/core/main.css";
 import "@fullcalendar/daygrid/main.css";
 
-document.addEventListener("DOMContentLoaded", function() {
-  var calendarEl = document.getElementById("calendar");
+const axios = require("axios");
 
-  var calendar = new Calendar(calendarEl, {
-    plugins: [ dayGridPlugin ]
-  });
+document.addEventListener("DOMContentLoaded", function () {
 
-  calendar.render();
+    // Create calendar
+    var calendarEl = document.getElementById("calendar");
+    var calendar = new Calendar(calendarEl, {
+        plugins: [dayGridPlugin, interactionPlugin],
+        dateClick: function (info) {
+            // Get the clicked date
+            let date = new Date(info.dateStr + 'T00:00:00');
+
+            // Send request to the API
+            axios({
+                method: "get",
+                url: "https://datazen.katren.ru/calendar/day/" + date.toDateString() + "/"
+            })
+            .then(res => {
+                let isholiday = res.data.holiday;
+                console.log(isholiday);
+            })
+            .catch(err => console.error(err));
+        },
+    });
+
+    calendar.render();
 });
